b8967b35e9c6ee1d069acf23e5391889a745bb84
Dmitrii Ivanov <3151020dim@mail.ru>
2023-01-22 13:04:12 +0300

    adding

---
+++ HW_DB2_diagram.sql
@@ -0,0 +1,46 @@
+create table if not exists genre (
+	id SERIAL primary key,
+	name VARCHAR(60) not null unique
+);
+
+create table if not exists singer (
+	id SERIAL primary key,
+	name VARCHAR(60) not null
+);
+
+create table if not exists album (
+	id SERIAL primary key,
+	name VARCHAR(60) not null unique,
+	creation_date date not null
+);
+
+create table if not exists song (
+	id SERIAL primary key,
+	name VARCHAR(60) not null,
+	lenght INTEGER not null check(lenght > 0),
+	album_id INTEGER references album(id)
+);
+
+create table if not exists treckcollection (
+	id SERIAL primary key,
+	name VARCHAR(60) not null unique,
+	creation_date date not null
+);
+
+create table if not exists genre_singer (
+	genre_id INTEGER references genre(id),
+	singer_id INTEGER references singer(id),
+	constraint pk primary key (genre_id, singer_id)
+);
+
+create table if not exists singer_album (
+	singer_id INTEGER references singer(id),
+	album_id INTEGER references album(id),
+	constraint pk2 primary key (singer_id, album_id)
+);
+
+create table if not exists song_treckcollection (
+	song_id INTEGER references song(id),
+	treckcollection_id INTEGER references treckcollection(id),
+	constraint pk3 primary key (song_id, treckcollection_id)
+);
